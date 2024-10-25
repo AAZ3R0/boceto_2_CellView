8df1b7c3d5f369983f6e393b764c21b0dee68f03
BRUNO GIBRAN REYES ALVAREZ <al205757@alumnos.uacj.mx>
2024-10-25 10:47:30 -0600

    [AGREGAR] navegacion usuario

--- boceto_2_CellView/controladores_de_pantallas/ControladorPantallaDelPost.swift
+++ boceto_2_CellView/controladores_de_pantallas/ControladorPantallaDelPost.swift
@@ -7,21 +7,16 @@
 
 import UIKit
 
-class ControladorPantallaDelPost: UIViewController{
+class ControladorPantallaDelPost: UIViewController, UICollectionViewDataSource{
+    private let identificador_de_zelda = "Celda comentario"
     
     let proveedor_publicaciones = ProveedorDePublicaciones.autoreferencia
     
     
     @IBOutlet weak var TItulo_de_publicacion: UILabel!
-    
     @IBOutlet weak var Cuerpo_de_publicacion: UILabel!
-    
-    
     @IBOutlet weak var Nombre_de_usuario: UILabel!
-    
-    
     @IBOutlet weak var Seccion_comentarios: UICollectionView!
-    
     public var id_publicacion: Int?
     private var publicacion: Post?
     private var usuario: Usuario?
@@ -34,10 +29,12 @@ class ControladorPantallaDelPost: UIViewController{
         
         
         let controlador_de_navegacion = self.navigationController as? mod_navegador_principal
-                controlador_de_navegacion?.activar_navigation_bar(activar: true)
+        controlador_de_navegacion?.activar_navigation_bar(activar: true)
+        
+        Seccion_comentarios.dataSource = self
         
         realizar_descarga_de_informacion()
-
+        
     }
     
     func realizar_descarga_de_informacion(){
@@ -46,19 +43,20 @@ class ControladorPantallaDelPost: UIViewController{
                 [weak self] (publicacion) in self?.publicacion = publicacion
                 DispatchQueue.main.async {
                     self?.dibujar_publicacion()
+                    self?.realizar_descarga_de_informacion()
                 }
             })
         }
         else if self.publicacion != nil{
             proveedor_publicaciones.obtener_usuario(id: publicacion!.userId, que_hacer_al_recibir: {
-                [weak self] (usuario) in self?.usuario = usuario 
+                [weak self] (usuario) in self?.usuario = usuario
                 DispatchQueue.main.async{
-                    self?.dibujar_publicacion()
+                    self?.dibujar_usuario()
                 }
-             })
+            })
         }
-    
-}
+        
+    }
     func dibujar_publicacion(){
         guard let publicacion_actual = self.publicacion else{
             return
@@ -70,5 +68,31 @@ class ControladorPantallaDelPost: UIViewController{
         
         TItulo_de_publicacion.text = publicacion_actual.title
         Cuerpo_de_publicacion.text = publicacion_actual.body
-            }}
+    }
+    
+    func dibujar_usuario(){
+        guard let usuario_actual = self.usuario else{
+            return
+        }
+        
+        Nombre_de_usuario.text = usuario_actual.username
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return lista_comentarios.count
+        }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            print("Aqui denberia hacer algo")
+            let celda = collectionView.dequeueReusableCell(withReuseIdentifier: identificador_de_zelda, for: indexPath)
+        
+            // Configure the cell
+            celda.tintColor = UIColor.green
+      
+            // print(self.lista_de_publicaciones)
+            
+            return celda
+        }
+    
+}
             
